249f490fc1d583b371214409186ae00e98ed312b
collins <collinswanjiru47@gmail.com>
2023-06-09 16:31:34 +0300

    redefining the routes on the server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 // Load environment variables
 dotenv.config();
 
+//connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   userNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express();
 
 // Set up middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -26,6 +27,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes
+const adminRoutes = require('./routes/admin');
+const tenantRoutes = require('./routes/tenants');
+const rentRoutes = require('./routes/rents');
+const chatRoutes = require('./routes/chat');
+
+
+
 app.use('/admin', adminRoutes);
 app.use('/tenants', tenantRoutes);
 app.use('/rents', rentRoutes);
