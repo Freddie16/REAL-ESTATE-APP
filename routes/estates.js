5e0c5343742adb14177e7c8afb35c7296ef5ca38
collins <collinswanjiru47@gmail.com>
2023-06-13 12:32:39 +0300

    Added an estate route to the routes

---
+++ routes/estates.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const estateController = require('../controllers/estateController');
+
+router.get('/', estateController.getAllEstates);
+router.post('/', estateController.createEstate);
+router.get('/:estateId', estateController.getEstateById);
+router.put('/:estateId', estateController.updateEstateById);
+router.delete('/:estateId', estateController.deleteEstateById);
+//router.get('/:estateId/houses', estateController.getHousesInEstate);
+
+module.exports = router;
