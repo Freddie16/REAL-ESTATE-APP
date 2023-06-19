86348a2f63b64d34046ff36e45f4e6e2300e635b
collins <collinswanjiru47@gmail.com>
2023-06-19 10:12:11 +0300

    modified models for tenant, estate and rentpayment

--- models/RentPayment.js
+++ models/RentPayment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const rentPaymentSchema = new mongoose.Schema({
   tenant: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: true },
   amount: { type: Number, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, required: true, default: Date.now },
   // Add other rent payment properties as needed
 });
 
--- models/Tenant.js
+++ models/Tenant.js
@@ -4,7 +4,6 @@ const tenantSchema = new mongoose.Schema({
     //properties of the tenant model
     name: { type: String, required: true},
     email: {type: String, required: true},
-    status: { type: String, enum: ['occupied', 'vacant'], default: 'occupied'},
 });
 
 const Tenant = mongoose.model('Tenant', tenantSchema);
