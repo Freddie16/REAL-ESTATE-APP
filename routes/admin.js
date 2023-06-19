29c068cd9ff4ab32ce3a8171b2e6ce03851280ef
collins <collinswanjiru47@gmail.com>
2023-06-19 10:28:10 +0300

    modified server.js to include the routes of the new components

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const adminController = require('../controllers/adminController');
 
 
 //Define routes for admin related functionality
-router.get('/dashboard', adminController.dashboard);
+//router.get('/dashboard', adminController.dashboard);
 router.get('/estates', adminController.estates);
 router.get('/houses', adminController.houses);
 router.get('/notifications', adminController.notifications);
-router.get('/rent-payments', adminController.rentPayments);
+router.get('/rentpayments', adminController.rentPayments);
 router.get('/tenants', adminController.tenants);
 
 module.exports = router;
--- routes/tenants.js
+++ routes/tenants.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const tenantController = require('../controllers/tenantController');
+const leaseController = require('../controllers/leaseController');
+
+// Tenant Routes
+router.get('/', tenantController.getAllTenants);
+router.post('/', tenantController.createTenant);
+router.get('/:tenantId', tenantController.getTenantById);
+router.put('/:tenantId', tenantController.updateTenantById);
+router.delete('/:tenantId', tenantController.deleteTenantById);
+
+// Lease Routes
+router.get('/leases', leaseController.getAllLeases);
+router.post('/create/leases', leaseController.createLease);
+router.get('/leases/:leaseId', leaseController.getLeaseById);
+router.put('/leases/:leaseId', leaseController.updateLeaseById);
+router.delete('/leases/:leaseId', leaseController.deleteLeaseById);
 
 
-//Define routes for tenant related functionality
-router.get('/lease', tenantController.lease);
-router.get('/pay-rent', tenantController.payRent);
-router.get('/rent-payments', tenantController.payRent);
-router.get('/vacant-houses', tenantController.vacanthouses);
+
+
 
 module.exports = router;
